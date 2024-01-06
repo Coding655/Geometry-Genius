96e21d624634cf526c5977eb3a0df20775db9316
coding655 <codingmaster655@gmail.com>
2024-01-06 11:42:03 +0600

    calculation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* Triangle Calculations */
 function getInputValueById(inputId){
     const inputField = document.getElementById(inputId);
     const inputFieldValue = inputField.value;
@@ -25,3 +26,44 @@ document.getElementById('triangle-btn').addEventListener('click', function(){
     setTextValueById('triangle-innerText', totalTriangleAmount);
 
 })
+
+document.getElementById('rectangle-btn').addEventListener('click', function(){
+    const rectangleInputLeftValue = getInputValueById('input-left-value');
+    const rectangleInputRightValue = getInputValueById('input-right-value');
+    const totalRectangleValue = rectangleInputLeftValue * rectangleInputRightValue;
+    const rectangleInnerText = getInnerTextValueById('rectangle-innerText');
+    const rectangleTotalAmount = totalRectangleValue + rectangleInnerText;
+    setTextValueById('rectangle-innerText', rectangleTotalAmount);
+})
+document.getElementById('parallelogram-btn').addEventListener('click', function(){
+    const parallelogramInputLeftValue = getInputValueById('para-input-left');
+    const parallelogramInputRightValue = getInputValueById('pera-input-right');
+    const parallelogramValue = parallelogramInputLeftValue * parallelogramInputRightValue;
+    const parallelogramInnerText = getInnerTextValueById('para-text');
+    const parallelogramTotalAmount = parallelogramValue + parallelogramInnerText;
+    setTextValueById('para-text', parallelogramTotalAmount);
+})
+document.getElementById('rhombus-btn').addEventListener('click', function(){
+   const  rhombusInputLeftValue = getInputValueById('rhombus-input-left');
+    const rhombusInputRightValue = getInputValueById('rhombus-input-right');
+    const rhombusValue =  rhombusInputLeftValue  *rhombusInputRightValue;
+    const rhombusInnerText =  getInnerTextValueById ('rhombus-innerText');
+    const rhombusTotalAmount = 0.5 *  rhombusValue + rhombusInnerText;
+    setTextValueById('rhombus-innerText', rhombusTotalAmount);
+})
+document.getElementById('pentagon-btn').addEventListener('click', function(){
+    const PentagonInputLeftValue = getInputValueById('pentagon-input-left');
+    const PentagonInputRightValue = getInputValueById('pentagon-input-right');
+    const PentagonValue =  PentagonInputLeftValue  * PentagonInputRightValue;
+    const PentagonInnerText =  getInnerTextValueById ('pentagon-innerText');
+    const PentagonTotalAmount = (0.5 *  PentagonValue)  + PentagonInnerText;
+    setTextValueById('pentagon-innerText', PentagonTotalAmount);
+})
+function setOneClick(){
+    const ellipseLeftInputValue = getInputValueById('ellipse-input-left');
+    const ellipseRightInputValue = getInputValueById('ellipse-input-right');
+    const ellipseValue = ellipseLeftInputValue * ellipseRightInputValue;
+    const ellipseInnerText =  getInnerTextValueById('ellipse-innerText');
+    const totalEllipseAmount = (3.1416 * ellipseValue) + ellipseInnerText;
+    setTextValueById('ellipse-innerText', totalEllipseAmount);
+}
